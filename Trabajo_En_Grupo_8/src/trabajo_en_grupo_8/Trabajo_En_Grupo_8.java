a150b70311d4b05efb0dea3d992c77b498fda4f8
Byron057 <Byronvelastegui07@gmail.com>
2026-06-05 19:00:03 -0500

    Agregar Scanner en el Proyecto

--- Trabajo_En_Grupo_8/src/trabajo_en_grupo_8/Trabajo_En_Grupo_8.java
+++ Trabajo_En_Grupo_8/src/trabajo_en_grupo_8/Trabajo_En_Grupo_8.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package trabajo_en_grupo_8;
+import java.util.Scanner;
 
 public class Trabajo_En_Grupo_8 {
 
@@ -10,11 +11,19 @@ public class Trabajo_En_Grupo_8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
         System.out.println("==========Trabajo Grupo 8==========");
         System.out.println("          Menu de Opciones          ");
         System.out.println("1.Sistema de Login");
         System.out.println("2.Numeros Positivos");
         System.out.println("3.Nota 0-10");
+        System.out.println("4.Cajero Automatico");
+        System.out.println("5.Validar Numeros Enteros");
+        
+        int opt = sc.nextInt();
+        
+        
+        
     }
     
 }
