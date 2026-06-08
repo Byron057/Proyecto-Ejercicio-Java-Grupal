b6f7db77370ea120bcaa4bdeabf992bd6b5475b3
Byron057 <Byronvelastegui07@gmail.com>
2026-06-07 21:29:17 -0500

    Corrección de Errores e Identación

--- Trabajo_En_Grupo_8/src/trabajo_en_grupo_8/Trabajo_En_Grupo_8.java
+++ Trabajo_En_Grupo_8/src/trabajo_en_grupo_8/Trabajo_En_Grupo_8.java
@@ -57,6 +57,8 @@ public class Trabajo_En_Grupo_8 {
                 case 5:
                     Numeros_pares_impares();
                     break;
+                case 6:
+                    
                 case 16:
                     System.out.println("Saliendo del Sistema....");
                     break;
@@ -68,26 +70,26 @@ public class Trabajo_En_Grupo_8 {
     }
     
      //Agregar Funciones Aqui para cada Ejercicio Segun el Menu de Opciones
-        static void sistema_login(){
-            Scanner sc = new Scanner(System.in);
-           int intentos = 0; 
-           String contraseña_real = "Trabajo5626";
+    public static void sistema_login(){
+        Scanner sc = new Scanner(System.in);
+        int intentos = 0; 
+        String contraseña_real = "Trabajo5626";
            
-            System.out.println("==========Sistema de Login==========");
-            System.out.println("");
-            System.out.println("Ingrese la contraseña");
-            String contraseña = sc.nextLine();
-            while(!contraseña_real.equals(contraseña)){
-                intentos++;
-                System.out.println("Contraseña Incorrecta");
-                System.out.println("Ingrese Nuevamente");
-                contraseña =sc.nextLine();
+        System.out.println("==========Sistema de Login==========");
+        System.out.println("");
+        System.out.println("Ingrese la contraseña");
+        String contraseña = sc.nextLine();
+        while(!contraseña_real.equals(contraseña)){
+            intentos++;
+            System.out.println("Contraseña Incorrecta");
+            System.out.println("Ingrese Nuevamente");
+            contraseña =sc.nextLine();
                 
-            }
-            System.out.println("===================================");
-            System.out.println("Contraseña Correcta");
-            System.out.println("Intentos Fallidos: " + intentos);
         }
+        System.out.println("===================================");
+        System.out.println("Contraseña Correcta");
+        System.out.println("Intentos Fallidos: " + intentos);
+    }
     public static void Numeros_positivos(){
         System.out.println("Ingresando al Sistema de Numeros positivos");
         
@@ -126,93 +128,94 @@ public class Trabajo_En_Grupo_8 {
     }
 
     public static void Nota_cerodiez(){
-    System.out.println("----Ingresando al sistema de cero y diez-----");
-    Scanner sc = new Scanner(System.in);
-    double nota;
+        System.out.println("----Ingresando al sistema de cero y diez-----");
+        Scanner sc = new Scanner(System.in);
+        double nota;
     
-    while (true){
-        System.out.println("Ingrese una nota entre 0 y 10 unicamente:  ");
-        nota=sc.nextDouble();
-        if(nota>=0 && nota <=10){
-            break;
+        while (true){
+            System.out.println("Ingrese una nota entre 0 y 10 unicamente:  ");
+            nota=sc.nextDouble();
+            if(nota>=0 && nota <=10){
+                break;
             
-        }
-        System.out.println("Nota invalida,ingrese unicamente entre 0 y 10");
-    } 
-    System.out.println("La nota valida que se registro es: "+nota);
- }
+            }
+            System.out.println("Nota invalida,ingrese unicamente entre 0 y 10");
+        } 
+        System.out.println("La nota valida que se registro es: "+nota);
+    }
  
 
-     public static void Cajero_automatico(){
+    public static void Cajero_automatico(){
         System.out.println("-----INGRESANDO AL SISTEMA DE CAJERO AUTOMATICO---"); 
         Scanner sc = new Scanner(System.in);
         String clave="4567";
         String ingreso;
         int intentos=0;
         
-         while(intentos < 3) {
+        while(intentos < 3) {
              System.out.println("Ingrese la clave de acceso: ");
-             
-             ingreso=sc.nextLine();
-             if(ingreso.equals(clave)){
-                 System.out.println("Bienvenido al cajero");
-                 break;
-             }
-             else{
+
+            ingreso=sc.nextLine();
+            if(ingreso.equals(clave)){
+                System.out.println("Bienvenido al cajero");
+                break;
+            }
+            else{
                  intentos ++;
-                 System.out.println("clave incorrecta");
-             }
-         }
+                System.out.println("clave incorrecta");
+            }
+        }
         if (intentos == 3){
-         System.out.println("acceso bloqueado");
-   }     
+        System.out.println("acceso bloqueado");
+        }     
     
-}
+    }
 
-
-     public static void Numeros_pares_impares(){
-       System.out.println("----Ingresando al sistema de numeros pares e impares-----");
+    public static void Numeros_pares_impares(){
+        System.out.println("----Ingresando al sistema de numeros pares e impares-----");
         Scanner sc = new Scanner(System.in);
           
-          int numero;
-          int pares=0;
-          int impares=0;
-          System.out.println("Ingrese numeros enteros y con 0 para terminar el sistema: ");
-          numero =sc.nextInt();
+        int numero;
+        int pares=0;
+        int impares=0;
+        System.out.println("Ingrese numeros enteros y con 0 para terminar el sistema: ");
+        numero =sc.nextInt();
           
-          while(numero != 0){
-              if (numero % 2 == 0){
-                  pares++;
+        while(numero != 0){
+            if (numero % 2 == 0){
+                pares++;      
+            }
+            else{
+                impares++;
+            }
+            numero =sc.nextInt();  
+        }
+        System.out.println("cantidad de numeros pares:"+pares);
+        System.out.println("cantidad de numeos impares: "+impares);
+    }  
               
-              }else{
-                   impares++;
-              }
-              numero =sc.nextInt();
-              
-          }
-              System.out.println("cantidad de numeros pares:"+pares);
-              System.out.println("cantidad de numeos impares: "+impares);
-              
-              
-              
-              System.out.println("ejercico 6 "); //Emerson Yanchapanta  
-              double total=0;
-              double ven;
-              int canti=0;
-              do{
-            System.out.println("ingrese la venta");
-            System.out.println("con 0 se detendra el sistema");
-            ven = sc.nextDouble();
-            if(ven !=0){
-                total+=ven;
-                canti++;
+    public static void Inventario_Tienda(){
+        Scanner sc= new Scanner(System.in);
+            System.out.println("ejercico 6 "); //Emerson Yanchapanta  
+            double total=0;
+            double ven;
+            int canti=0;
+            do{
+                System.out.println("ingrese la venta");
+                System.out.println("con 0 se detendra el sistema");
+                ven = sc.nextDouble();
+                if(ven !=0){
+                    total+=ven;
+                    canti++;
                 }
-        }while(ven !=0);
+            }while(ven !=0);
         System.out.println("Total vendido: "+total);
         System.out.println("Cantidad de ventas: "+canti);
         
+    }
         
-        
+    public static void Numeros_Positivos(){
+        Scanner sc= new Scanner(System.in);
         System.out.println("ejercico 7 "); //Emerson Yanchapanta  
         int cantidad = 0;
         int numero12=0;
@@ -227,8 +230,10 @@ public class Trabajo_En_Grupo_8 {
         System.out.println("Cantidad de numeros ingresados: " + cantidad);
         
         
+    }   
         
-        
+    public static void Tabla_multiplicar(){ 
+        Scanner sc= new Scanner(System.in);
         System.out.println("ejercico 8 "); //Emerson Yanchapanta  
         int cons=0;
         System.out.println("Ingrese un numero positivo");
@@ -236,11 +241,12 @@ public class Trabajo_En_Grupo_8 {
         System.out.println("Tabla de multiplicar del " +cons);
         for (int i=1; i<=20; i++){
             System.out.println(cons + " x " + i + " = " + (cons * i));
-            }
+        }
         
+    }
         
-        
-        
+    public static void Calcular_factorial(){ 
+        Scanner sc= new Scanner(System.in);
         System.out.println("ejercico 9 "); //Emerson Yanchapanta 
         System.out.println("ejercico  8+1=9 "); //Emerson Yanchapanta 
         System.out.println("ingrese un numero: ");
@@ -259,7 +265,7 @@ public class Trabajo_En_Grupo_8 {
          System.out.println("FIN DE MI PARTE EMERSON YANCHAPANTA : ");
       
        }
-      public static void Nueros_positivos_negativos(){
+    public static void Nueros_positivos_negativos(){
        System.out.println("----ejercicio 10-----");//aracely toapanta 
         Scanner sc = new Scanner(System.in);  
          int num;
@@ -281,9 +287,9 @@ public class Trabajo_En_Grupo_8 {
         System.out.println("Cantidad de negativos: " + negativos);
         System.out.println("Cantidad de ceros: " + ceros);
         
-         }
+    }
          
-       public static void Promedio_de_20_estudiantes(){
+    public static void Promedio_de_20_estudiantes(){
         System.out.println("----ejercicio 11-----");//aracely toapanta 
         Scanner sc = new Scanner(System.in); 
         double nota;
@@ -299,8 +305,8 @@ public class Trabajo_En_Grupo_8 {
             promedio = suma / 20;
             System.out.println("El promedio general del curso es: " + promedio);
             
-        }
-       public static void Secuencias_de_Fibonacci(){
+    }
+    public static void Secuencias_de_Fibonacci(){
         System.out.println("----ejercicio 12-----");//aracely toapanta 
         Scanner sc = new Scanner(System.in);
         int n;
@@ -314,10 +320,10 @@ public class Trabajo_En_Grupo_8 {
             a = b;
             b = c;
         }
-       
-                    
     }
-       public static void Numero_maoy_y_menor(){
+                    
+    
+    public static void Numero_maoy_y_menor(){
         System.out.println("----ejercicio 13-----");//aracely toapanta 
         Scanner sc = new Scanner(System.in);
         int num;
@@ -330,22 +336,24 @@ public class Trabajo_En_Grupo_8 {
             if (i == 1) {
                 mayor = num;
                 menor = num;
-            } else {
+            } 
+            else {
                 if (num > mayor) {
                     mayor = num;
                 }
 
                 if (num < menor) {
                     menor = num;
-      }
-       }
+                }
+            }
        }
         System.out.println("El número mayor es: " + mayor);
         System.out.println("El número menor es: " + menor);
-           System.out.println("parte de aracely");
-        }
-       }
-       
+        System.out.println("parte de aracely");
+    }
+
+}
+    
        
         
         
